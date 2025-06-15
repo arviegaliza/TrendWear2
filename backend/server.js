212f2f70abb97e90aa0a1a1fbb1c2917e4212811
arviegaliza <arviegaliza4@gmail.com>
2025-06-15 22:58:54 +0800

    update

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,14 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
+
 
 // Check MySQL connection
 pool.getConnection((err, connection) => {
